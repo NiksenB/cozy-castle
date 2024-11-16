a75b9bf7227c2c1f9f44d8500b89144cc6b4d5de
NiksenB <90034452+NiksenB@users.noreply.github.com>
2024-11-17 00:03:03 +0100

    Finish animation

--- CozyCastle/Assets/Scripts/PlayerMovementScript.cs
+++ CozyCastle/Assets/Scripts/PlayerMovementScript.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerMovementScript : MonoBehaviour
 {
     public float speed;
-    public Rigidbody2D rb;
+    public Animator animator;
 
     public void Update()
     {
@@ -11,6 +11,26 @@ public class PlayerMovementScript : MonoBehaviour
         float v = Input.GetAxisRaw("Vertical");
 
         Vector3 direction = new Vector3(h, v, 0);
+
+        AnimateMovement(direction);
+
         transform.position += direction * speed * Time.deltaTime;
+    }
+
+    public void AnimateMovement(Vector3 direction) 
+    {
+        if (animator != null)
+        {
+            if (direction.magnitude > 0)
+            {
+                animator.SetBool("isMoving", true);
+                animator.SetFloat("horizontal", direction.x);
+                animator.SetFloat("vertical", direction.y);
+            }
+            else 
+            {
+                animator.SetBool("isMoving", false);
+            }
+        }
     }
 }
