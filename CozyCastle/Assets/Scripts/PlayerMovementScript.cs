16e5ec9e3febf193cdff463f680e3cf5ddf4d4c6
Nikoline Burman <nibu@itu.dk>
2024-11-16 21:51:00 +0100

    WIP animations with placeholder

--- CozyCastle/Assets/Scripts/PlayerMovementScript.cs
+++ CozyCastle/Assets/Scripts/PlayerMovementScript.cs
@@ -2,14 +2,9 @@ using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviour
 {
+    public float speed;
     public Rigidbody2D rb;
-    public Sprite princess;
-    public Sprite princess_right_1;
-    public Sprite princess_right_2;
-    public float speed = 100;
-
-    private SpriteRenderer mySpriteRenderer;
-    private bool step;
+    public Sprite mySprite;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,35 +14,11 @@ public class PlayerMovementScript : MonoBehaviour
 
     public void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
 
-        Vector3 tempVect = new Vector3(h, v, 0);
-        tempVect = tempVect.normalized * speed * Time.deltaTime;
+        Vector3 direction = new Vector3(h, v, 0);
+        transform.position += direction * speed * Time.deltaTime;
 
-        mySpriteRenderer.flipX = h < 0;
-        UpdateSprite(h);
-        rb.MovePosition(rb.transform.position + tempVect);
-    }
-
-    public void UpdateSprite(float horizontalMov)
-    {
-        if (horizontalMov == 0) 
-        {
-            mySpriteRenderer.sprite = princess;
-        } 
-        else 
-        {
-            if (step) {
-                step = false;
-                mySpriteRenderer.sprite = princess_right_2;
-            } else 
-            {
-                step = true;
-                mySpriteRenderer.sprite = princess_right_1;
-            }
-        }
-
-        gameObject.GetComponent<SpriteRenderer>().sprite = mySpriteRenderer.sprite;
     }
 }
