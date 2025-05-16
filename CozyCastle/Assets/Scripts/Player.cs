1368a6990b92fa0ca679f6681894476a0add753d
Nikoline Burman <nikoline.burman@gmail.com>
2025-05-17 00:08:58 +0200

    Feat : Magic wand plays animation when used

--- CozyCastle/Assets/Scripts/Player.cs
+++ CozyCastle/Assets/Scripts/Player.cs
@@ -5,11 +5,12 @@ public class Player : MonoBehaviour
     public InventoryManager inventoryManager;
     private TileManager tileManager;
     private PlayerStats playerStats;
+    private PlayerMovementScript movementScript;
 
-    // Called when the game starts
     private void Awake()
     {
         inventoryManager = GetComponent<InventoryManager>();
+        movementScript = GetComponent<PlayerMovementScript>();
     }
 
     private void Start()
@@ -26,10 +27,6 @@ public class Player : MonoBehaviour
             {
                 Vector3Int position = new((int)transform.position.x, (int)transform.position.y, 0);
 
-                // if (tileManager.IsInteractable(position))
-                // {
-                //     TryInteract(position);
-                // }
                 string tileName = tileManager.GetTileName(position);
 
                 if (!string.IsNullOrEmpty(tileName))
@@ -52,33 +49,39 @@ public class Player : MonoBehaviour
         float randomDistance = Random.Range(minDistance, dropRadius);
         Vector2 spawnOffset = randomDirection * randomDistance;
         Vector2 spawnLocation = transform.position + (Vector3)spawnOffset;
-        
+
         // Spawn in world.
         float randomRotation = Random.Range(0f, 360f);
         Item droppedItem = Instantiate(item, spawnLocation + spawnOffset, Quaternion.Euler(0f, 0f, randomRotation));
-        
+
         droppedItem.rigidbody2d.AddForce(spawnOffset.normalized * 2f, ForceMode2D.Impulse);
 
         droppedItem.rigidbody2d.linearDamping = 4f;     // Linear drag slows down movement
         droppedItem.rigidbody2d.angularDamping = 2f;    // Angular drag slows down rotation
-        
+
         Debug.Log("Player removed inventory item " + item.data.itemName);
     }
 
     public void DropItem(Item item, int numToDrop)
     {
-        for(int i = 0; i < numToDrop; i++)
+        for (int i = 0; i < numToDrop; i++)
         {
             DropItem(item);
-        } 
+        }
     }
-
 
     private void TryInteract(Vector3Int position)
     {
-        if (playerStats.TryUseMana(25)) 
+        if (playerStats.TryUseMana(25))
         {
-            tileManager.SetInteracted(position);
+            movementScript.PlayWandSwingAnimation();
+            StartCoroutine(DelayedInteract(position, 0.5f));
         }
+    }
+    
+    private System.Collections.IEnumerator DelayedInteract(Vector3Int position, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        tileManager.SetInteracted(position);
     }
 }
--- CozyCastle/Assets/Scripts/PlayerMovement.cs
+++ CozyCastle/Assets/Scripts/PlayerMovement.cs
@@ -4,17 +4,22 @@ public class PlayerMovementScript : MonoBehaviour
 {
     public float speed;
     public Animator animator;
+    public GameObject wand;
     private Vector3 direction;
 
     public void Update()
     {
-        // Get player input
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-
         direction = new Vector3(h, v, 0);
 
         AnimateMovement(direction);
+
+        bool wandEquipped = GameManager.gameInstance.player.inventoryManager.toolbar.selectedSlot.itemName == "Magic Wand";
+        if (wand != null)
+        {
+            wand.SetActive(wandEquipped);
+        }
     }
 
     // Called after collisions are calculated
@@ -24,7 +29,7 @@ public class PlayerMovementScript : MonoBehaviour
         transform.position += direction * speed * Time.deltaTime;
     }
 
-    public void AnimateMovement(Vector3 direction) 
+    public void AnimateMovement(Vector3 direction)
     {
         if (animator != null)
         {
@@ -34,9 +39,21 @@ public class PlayerMovementScript : MonoBehaviour
                 animator.SetFloat("horizontal", direction.x);
                 animator.SetFloat("vertical", direction.y);
             }
-            else 
+            else
             {
                 animator.SetBool("isMoving", false);
+            }
+        }
+    }
+
+    public void PlayWandSwingAnimation()
+    {
+        if (wand != null)
+        {
+            Animator wandAnimator = wand.GetComponent<Animator>();
+            if (wandAnimator != null)
+            {
+                wandAnimator.SetTrigger("swingWand"); 
             }
         }
     }
