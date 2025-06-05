6c5bbf6b2ed96d31ead7c585673b70e76513f777
Nikoline Burman <nikoline.burman@gmail.com>
2025-06-05 14:23:59 +0200

    Feat : Add bunny

---
+++ CozyCastle/Assets/Scripts/Creatures/Bunny.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Bunny : Creature
+{
+
+    private Rigidbody2D myRigidbody;
+    public GameObject loveBubble;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        if (myRigidbody == null) Debug.LogError("Rigidbody2D component not found on the Goat object.");
+
+        animator = GetComponentInChildren<Animator>();
+        if (animator == null) Debug.LogError("Animator component not found on the Goat object.");
+        else SetAnimator(animator);
+
+        if (loveBubble == null) Debug.LogError("Love Bubble prefab is not assigned. Please assign it in the inspector.");
+        else SetLoveBubble(loveBubble);
+
+        var player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            playerPosition = player.transform;
+        }
+        else
+        {
+            Debug.LogWarning("Player object not found. Ensure the player has the 'Player' tag.");
+            playerPosition = null;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (playerPosition != null)
+        {
+            bool isPlayerVisible = IsPlayerVisible();
+            UpdatePlayerVisibility(isPlayerVisible);
+            if (isPlayerVisible)
+            {
+                ReactToPlayer(myRigidbody);
+            }
+        }
+    }
+
+    override
+    public bool IsAlert()
+    {
+        return true;
+    }
+}
