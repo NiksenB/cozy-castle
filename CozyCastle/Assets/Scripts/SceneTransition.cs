9925bd12ba98b76385b1380b5f1c2214d5e7fdd6
Nikoline Burman <nikoline.burman@gmail.com>
2026-02-02 22:02:49 +0100

    Feat : Fade transitions

--- CozyCastle/Assets/Scripts/SceneTransition.cs
+++ CozyCastle/Assets/Scripts/SceneTransition.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,7 +8,18 @@ public class SceneTransition : MonoBehaviour
     public string sceneToLoadName;
     public Vector3 playerPosition;
     public VectorValue playerPositionValue;
+    public GameObject fadeInPanel;
+    public GameObject fadeOutPanel;
 
+    private void Awake()
+    {
+        if (fadeInPanel != null)
+        {
+            GameObject panel = Instantiate(fadeInPanel);
+            Destroy(panel, 1);
+        }
+    }
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !collision.isTrigger)
@@ -20,15 +32,42 @@ public class SceneTransition : MonoBehaviour
 
             playerPositionValue.initialValue = playerPosition;
 
-            // Load the scene using the scene index or name
-            if (sceneToLoadIndex >= 0)
-            {
-                SceneManager.LoadScene(sceneToLoadIndex);
-            }
-            else if (!string.IsNullOrEmpty(sceneToLoadName))
-            {
-                SceneManager.LoadScene(sceneToLoadName);
-            }
+            StartCoroutine(FadeOut());
+
+            
         }
     }
+
+    public IEnumerator FadeOut()
+    {
+        if (fadeOutPanel != null)
+        {
+            Instantiate(fadeOutPanel);
+        }
+        yield return new WaitForSeconds(0.6f);
+
+        AsyncOperation async = LoadSceneAsync();
+        
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    private AsyncOperation LoadSceneAsync()
+    {
+        // Load the scene using the scene index or name
+        if (sceneToLoadIndex >= 0)
+        {
+            return SceneManager.LoadSceneAsync(sceneToLoadIndex);
+        }
+        
+        if (!string.IsNullOrEmpty(sceneToLoadName))
+        {
+            return SceneManager.LoadSceneAsync(sceneToLoadName);
+        }
+
+        return null;
+    }
+
 }
