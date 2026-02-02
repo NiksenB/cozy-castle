dc91ac32a6b8710b9dca6253a1d90fa58cc48c95
Nikoline Burman <nikoline.burman@gmail.com>
2026-02-02 20:20:44 +0100

    Fix : Player Spawn Position on Scene Transition

--- CozyCastle/Assets/Scripts/ScriptableObjects/VectorValue.cs
+++ CozyCastle/Assets/Scripts/ScriptableObjects/VectorValue.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 
 [CreateAssetMenu]
-public class VectorValue : ScriptableObject
+public class VectorValue : ScriptableObject, ISerializationCallbackReceiver
 {
-    public Vector3 value;
-
-    // Constructor to initialize the vector value
-    public VectorValue(Vector3 initialValue)
+    public Vector3 initialValue;
+    public Vector3 defaultValue;
+    
+    public void OnAfterDeserialize()
     {
-        value = initialValue;
+        initialValue = defaultValue;
+    }
+
+    public void OnBeforeSerialize()
+    {
+        
     }
 }
